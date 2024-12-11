e4eefc62f473644abc2c73e167775c55d47daeee
Jardel Bordignon <jardel1101@gmail.com>
2024-12-11 13:53:23 -0300

    feat: add fastify

--- nestjs-api/src/main.ts
+++ nestjs-api/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const port = process.env.PORT || 3000;
   const logger = new Logger('API');
 
