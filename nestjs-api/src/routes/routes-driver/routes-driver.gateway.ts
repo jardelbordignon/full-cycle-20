c9d66958a4d990b4ffbc821db8368a0cb454dae6
Jardel Bordignon <jardel1101@gmail.com>
2024-12-15 07:34:43 -0300

    feat: emitting multiple messages

--- nestjs-api/src/routes/routes-driver/routes-driver.gateway.ts
+++ nestjs-api/src/routes/routes-driver/routes-driver.gateway.ts
@@ -1,4 +1,5 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { RoutesService } from '../routes.service';
 
 @WebSocketGateway({
   cors: {
@@ -12,11 +13,39 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
   },
 })
 export class RoutesDriverGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  constructor(private routesService: RoutesService) {}
+
+  @SubscribeMessage('client:new-points')
+  async handleNewPoints(client: any, payload: any) {
     // client.emit('xyz', payload); // envia para quem enviou a mensagem
     // client.broadcast.emit('xyz', payload); // envia para todos que estiverem conectados ao evento xyz
-    console.log('payload:', payload);
-    return 'Hello world!';
+    const { routeId } = payload
+    const route = await this.routesService.findOne(routeId)
+
+    if (!route) {
+      throw new Error('Route does not exists')
+    }
+
+    // @ts-expect-error - routes has not been defined
+    const { steps } = route.directions!.routes[0].legs[0]
+    for (const step of steps) {
+      const startData = {
+        ...step.start_location,
+        routeId,
+      }
+      client.emit(`server:new-points/${routeId}:list`, startData)
+      client.broadcast.emit('server:new-points:list', startData)
+      await sleep(2000)
+
+      const endData = {
+        ...step.end_location,
+        routeId,
+      }
+      client.emit(`server:new-points/${routeId}:list`, endData)
+      client.broadcast.emit('server:new-points:list', endData)
+      await sleep(2000)
+    }
   }
 }
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
