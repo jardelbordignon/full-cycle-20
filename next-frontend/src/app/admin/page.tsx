1f0b4823717f9eeaf3ab711c7416f820382be018
Jardel Bordignon <jardel1101@gmail.com>
2024-12-15 07:32:13 -0300

    feat: admin page

--- next-frontend/src/app/admin/page.tsx
+++ next-frontend/src/app/admin/page.tsx
@@ -1,11 +1,55 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "@/hooks/use-map";
+import { socket } from "@/utils/socket-io";
+import { fetchGetRoute } from "@/services/requests";
+
+type Payload = {
+  routeId: string 
+  lat: number 
+  lng: number 
+}
 
 export default function Admin() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
+  const map = useMap(mapContainerRef);
+
+  useEffect(() => {
+    if (!map) return;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    socket.disconnected ? socket.connect() : socket.offAny();
+
+    socket.on("server:new-points:list", async (data: Payload) => {
+      const { routeId, lat, lng } = data;
+
+      if (map.hasRoute(routeId)) {
+        map.moveCar(routeId, { lat, lng });
+      } else {
+        const getRouteResponse = await fetchGetRoute(routeId)
+        const route = await getRouteResponse.json()
+        const { start_location, end_location } = route.directions.routes[0].legs[0];
+        
+        map.addRouteWithIcons({
+          routeId,
+          carMarkerOptions: {
+            position: start_location,
+          },
+          startMarkerOptions: {
+            position: start_location,
+          },
+          endMarkerOptions: {
+            position: end_location,
+          },
+        });
+      }
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [map]);
 
   return <div className="w-full h-full min-h-screen" ref={mapContainerRef} />;
 }
